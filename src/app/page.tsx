5fe2d0964c5a23080200fb228eebd781cfa219cb
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2024-11-30 02:43:29 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,29 @@
 import Image from "next/image";
-import Hero from "./components/Landing Page/hero";
-import Features from "./components/Landing Page/feature";
-// import Quiz from "./components/Onboard/onboard";
-import HowItWorks from "./components/Landing Page/work";
-import RoleToggle from "./components/Landing Page/toggle";
-import InteractiveDemo from "./components/Landing Page/video";
-// import Dahboard from "./components/Onboard/onboard";
-import MissionSection from "./components/Landing Page/MissionSection";
-import FAQSection from "./components/Landing Page/Faqs";
-import CTASection from "./components/Landing Page/cta";
-import HeroSection from "./components/Landing Page/hero";
-import Footer from "./components/Landing Page/footer";
-// import Navbar from "./components/Navbar/navbar"
-// import Sidebar from "./components/SiderBar/page";
+import dynamic from 'next/dynamic';
+
+// Dynamically import components
+const HeroSection = dynamic(() => import("./components/Landing Page/hero"));
+const Features = dynamic(() => import("./components/Landing Page/feature")); 
+const HowItWorks = dynamic(() => import("./components/Landing Page/work"));
+const RoleToggle = dynamic(() => import("./components/Landing Page/toggle"));
+const InteractiveDemo = dynamic(() => import("./components/Landing Page/video"));
+const MissionSection = dynamic(() => import("./components/Landing Page/MissionSection"));
+const FAQSection = dynamic(() => import("./components/Landing Page/Faqs"));
+const CTASection = dynamic(() => import("./components/Landing Page/cta"));
+const Footer = dynamic(() => import("./components/Landing Page/footer"));
 
 export default function Home() {
   return (
     <>
- <HeroSection/>
-   <Features/> 
-    <MissionSection/>
- <HowItWorks/>
-    <RoleToggle/>
-    <InteractiveDemo/>
-    <FAQSection/>
-     <CTASection/>  
-     <Footer/>
-    
-
+      <HeroSection />
+      <Features />
+      <MissionSection />
+      <HowItWorks />
+      <RoleToggle />
+      <InteractiveDemo />
+      <FAQSection />
+      <CTASection />
+      <Footer />
     </>
   );
 }

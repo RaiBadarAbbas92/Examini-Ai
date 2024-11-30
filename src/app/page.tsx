3718dd790ec3aa53c0b8099660ad74d5e44d0999
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2024-11-30 02:47:18 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
-import Image from "next/image";
+"use client";
 import dynamic from 'next/dynamic';
 
 // Dynamically import components
-const HeroSection = dynamic(() => import("./components/Landing Page/hero"));
-const Features = dynamic(() => import("./components/Landing Page/feature")); 
-const HowItWorks = dynamic(() => import("./components/Landing Page/work"));
-const RoleToggle = dynamic(() => import("./components/Landing Page/toggle"));
-const InteractiveDemo = dynamic(() => import("./components/Landing Page/video"));
-const MissionSection = dynamic(() => import("./components/Landing Page/MissionSection"));
-const FAQSection = dynamic(() => import("./components/Landing Page/Faqs"));
-const CTASection = dynamic(() => import("./components/Landing Page/cta"));
-const Footer = dynamic(() => import("./components/Landing Page/footer"));
+const HeroSection = dynamic(() => import("./components/Landing Page/hero"), {
+  ssr: false
+});
+const Features = dynamic(() => import("./components/Landing Page/feature"), {
+  ssr: false
+}); 
+const HowItWorks = dynamic(() => import("./components/Landing Page/work"), {
+  ssr: false
+});
+const RoleToggle = dynamic(() => import("./components/Landing Page/toggle"), {
+  ssr: false
+});
+const InteractiveDemo = dynamic(() => import("./components/Landing Page/video"), {
+  ssr: false
+});
+const MissionSection = dynamic(() => import("./components/Landing Page/MissionSection"), {
+  ssr: false
+});
+const FAQSection = dynamic(() => import("./components/Landing Page/Faqs"), {
+  ssr: false
+});
+const CTASection = dynamic(() => import("./components/Landing Page/cta"), {
+  ssr: false
+});
+const Footer = dynamic(() => import("./components/Landing Page/footer"), {
+  ssr: false
+});
 
 export default function Home() {
   return (
